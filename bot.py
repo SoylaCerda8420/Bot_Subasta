d0b6521b1ec82cbf2b790533a29315c51e1dc415
SoylaCerda8420 <joackocisternas@gmail.com>
2026-05-18 21:58:51 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -451,7 +451,7 @@ class ConfirmarSubastaView(discord.ui.View):
                 pass
 
             await subasta.canal.send(
-                "@here 🔨 SUBASTA ACTIVA"
+                "# @here 🔨 SUBASTA ACTIVA"
             )
             
 # ==================================================
